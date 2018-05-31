60420bf83ea3b273dbe90c2c28b90daf2da9a5d3
Andrii Ivaniv <unixvb@gmail.com>
2018-05-31 16:07:25 +0300

    new header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app/app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
